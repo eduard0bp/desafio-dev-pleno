98cb63862aaee903f51b3bc4561d61495dc5a1d8
Eduardo Batista <eduardo4gac@gmail.com>
2026-08-26 12:17:03 -0300

    chore: scaffold frontend with vite, mantine and tanstack query

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import { Container, Title } from '@mantine/core';
+
+export default function App() {
+  return (
+    <Container size="md" py="xl">
+      <Title order={1}>Falaê! Avaliações</Title>
+    </Container>
+  );
+}
